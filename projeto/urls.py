268848a7e3057417505a52a570e12344e2a25ec1
Gabriel Benicio <gabrielvitor083@gmail.com>
2023-12-23 17:15:44 -0300

    Add URLS in app

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from recipes.views import home, contato
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',  home),
+    path('contato/', contato)
 ]
